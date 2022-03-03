b97e209ef91ff99885e819097380a389140edd67
raview69 <raviwicaksono@aol.com>
2022-03-04 03:10:02 +0700

    fix main

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
+import os
 import MailboxValidator
+from datetime import date
+
+
+
+
+today = date.today()
+d4 = today.strftime("%b-%d-%Y")
+dat = str(d4)
+os.makedirs(dat)
+
 
 l = input("input list file : ")
 a = open('api.txt', 'r').read().splitlines()
@@ -9,10 +20,11 @@ ad = str(ba)
 lo = ac // c
 li = lo+1
 
+
 for i in range(1, li+1):
     n = i * c
     n2 = n - c
-    ap = open(f'list{i}.txt', 'a+')
+    ap = open(os.path.join(dat, f'list{i}.txt'), 'a+')
     for line in ba[n2:n]:
         ap.write(line + '\n')
     
