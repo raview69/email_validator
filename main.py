618446c0d3a764b0571a15a32aff1e985b653ed8
raview69 <raviwicaksono@aol.com>
2022-03-04 01:40:16 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import MailboxValidator
+
+
+a = open('api.txt', 'r').read().splitlines()
+b = open('list.txt', 'r').read().splitlines()
+for i in range(0, len(a)):
+    print('pinter')
+    for j in range(0, len(b)):
+        print('saya')
+
