258b119f1218c3d0e5ce0602f9d89465a1d709b7
raview69 <raviwicaksono@aol.com>
2022-03-04 02:40:39 +0700

    fix main.py

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import MailboxValidator
 
-
+l = input("input list file : ")
 a = open('api.txt', 'r').read().splitlines()
-b = open('list.txt', 'r').read().splitlines()
-for i in range(0, len(a)):
-    print('pinter')
-    for j in range(0, len(b)):
-        print('saya')
+c = int(input("how many check 1 api : "))
+ba = open(l, 'r').read().splitlines()
+ac = len(ba)
+ad = str(ba)
+lo = ac // c
+li = lo+1
+
+for i in range(1, li+1):
+    n = i * c
+    n2 = n - c
+    ap = open(f'list{i}.txt', 'a+')
+    for line in ba[n2:n]:
+        ap.write(line + '\n')
+    
+
 
