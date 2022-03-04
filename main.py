1949d0eae1fae7d60766f1decd33575dc6ea0db6
raview69 <raviwicaksono@aol.com>
2022-03-04 08:37:15 +0700

    fix main

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 import MailboxValidator
 from datetime import date
-
-
+from pathlib import Path
+from termcolor import colored
 
 # make directory
 today = date.today()
@@ -29,18 +29,26 @@ for i in range(1, li+1):
     for line in ba[n2:n]:
         ap.write(line + '\n')
 
-# excute the list
+# execute the list
 for i in range(0, len(a)):
     mbv = MailboxValidator.EmailValidation(a[0+i])
-    lb = open(os.path.join(dat, f'list{i+1}.txt'), 'r')
-    for j in range(0, len(lb)):
-        results = mbv.validate_email(lb[0+j])
-        if results is None:
-            print("Error connecting to API.\n")
-        elif results['error_code'] == '':
-            print('email_address = ' + results['email_address'] + "\n")
-            print('mailboxvalidator_score = ' + str(results['mailboxvalidator_score']) + "\n")
-        else:
-            print('error_code = ' + results['error_code'] + "\n")
+    data_folder = Path(dat)
+    lb = data_folder / f'list{i+1}.txt'
+    fu = open(lb).read().splitlines()
+    for j in range(0, len(fu)):
+            results = mbv.validate_email(fu[0+j])
+            if results is None:
+                print("Error connecting to API.\n")
+            elif results['error_code'] == '':
+                ase = float(results['mailboxvalidator_score'])
+                if (ase > 0.7):
+                    print(colored(results['email_address'] + ' - score: ' + str(results['mailboxvalidator_score']), 'green'))
+                    ar = open('result.txt', 'a+')
+                    ar.write(fu[0+j] + '\n')
+                    ar.close()
+                else:
+                    print(colored(results['email_address'] + ' - score: ' + str(results['mailboxvalidator_score']), 'red'))
+            else:
+                print('error_code = ' + results['error_code'] + "\n")
             
 
